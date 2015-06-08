6d8fd9d9f2994050d89709253d531ac3ce41cbf0
Marko Ivanovic <ivanovic.marko@yandex.com>
2015-06-08 17:15:33 +0200

    initial project setup

---
+++ src/net/etfbl/kki/MJTest.java
@@ -0,0 +1,13 @@
+package net.etfbl.kki;
+
+public class MJTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/net/etfbl/kki/sym.java
@@ -0,0 +1,5 @@
+package net.etfbl.kki;
+
+public class sym {
+
+}
